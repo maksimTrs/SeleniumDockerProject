0fadbea39f1a302306b8c7178105d1ddd820f64a
Maksim T <roronoazorroippo@gmail.com>
2022-12-29 01:57:01 +0400

    Add new tests with Selenium Grid part 2

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,24 +5,54 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 
-import static tests.LocalDriverManager.BrowserType.LOCAL;
+import java.net.MalformedURLException;
+
+import static tests.LocalDriverManager.BrowserType.SELENIUM_GRID;
 
 public class BaseTest {
 
+    private static final String HOST = "localhost";
     protected WebDriver driver;
-
 
     @BeforeTest
     @Parameters({"browserType"})
-    protected void setUp(String browserType) {
+    protected void setUp(String browserType) throws MalformedURLException {
         //WebDriverManager.chromedriver().setup();
         LocalDriverManager localDriverManager = new LocalDriverManager();
-        driver = localDriverManager.createInstance(browserType, LOCAL);
+        driver = localDriverManager.createInstance(browserType, SELENIUM_GRID, HOST);
 
         // driver = WebDriverManager.chromedriver().create();  //new ChromeDriver();
 
         driver.manage().window().maximize();
     }
+
+
+   /* @BeforeTest
+    public void setupDriver(ITestContext ctx) throws MalformedURLException {
+        // BROWSER => chrome / firefox
+        // HUB_HOST => localhost / 10.0.1.3 / hostname
+
+        String host = "localhost";
+        MutableCapabilities dc;
+
+        if(System.getProperty("BROWSER") != null &&
+                System.getProperty("BROWSER").equalsIgnoreCase("firefox")){
+            dc = new FirefoxOptions();
+        }else{
+            dc = new ChromeOptions();
+        }
+
+        if(System.getProperty("HUB_HOST") != null){
+            host = System.getProperty("HUB_HOST");
+        }
+
+        String testName = ctx.getCurrentXmlTest().getName();
+
+        String completeUrl = "http://" + host + ":4444/wd/hub";
+        dc.setCapability("name", testName);
+        this.driver = new RemoteWebDriver(new URL(completeUrl), dc);
+    }*/
+
 
     @AfterTest
     protected void teardown() {
--- src/test/java/tests/LocalDriverManager.java
+++ src/test/java/tests/LocalDriverManager.java
@@ -4,18 +4,31 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.bonigarcia.wdm.config.DriverManagerType;
 import org.openqa.selenium.WebDriver;
 
+import java.net.MalformedURLException;
+
 public class LocalDriverManager {
 
     // to make this work just pass the browser name, that must match from the DriverManagerType class
-    public WebDriver createInstance(String browser, BrowserType browserType) {
-        String path = "http://localhost:4444/wd/hub";
+    public WebDriver createInstance(String browser, BrowserType browserType, String host) throws MalformedURLException {
+        String path = "http://" + host + ":4444/wd/hub";
         WebDriver driver = null;
+
 
         DriverManagerType driverManagerType = DriverManagerType.valueOf(browser.toUpperCase());
         //  Class<?> driverClass = Class.forName(driverManagerType.browserClass());
 
         if (browserType.equals(BrowserType.SELENIUM_GRID)) {
             driver = WebDriverManager.getInstance(driverManagerType).remoteAddress(path).create();
+
+//            if (driverManagerType.equals(DriverManagerType.CHROME)) {
+//                URL gridUrl = new URL(path);
+//                ChromeOptions options = new ChromeOptions();
+//                options.setCapability("se:recordVideo", true);
+//                options.setCapability("se:timeZone", "US/Pacific");
+//                options.setCapability("se:screenResolution", "1920x1080");
+//                RemoteWebDriver webDriver = new RemoteWebDriver(gridUrl, options);
+//                return webDriver;
+//            }
         } else if (browserType.equals(BrowserType.LOCAL)) {
             driver = WebDriverManager.getInstance(driverManagerType).create();
         }
